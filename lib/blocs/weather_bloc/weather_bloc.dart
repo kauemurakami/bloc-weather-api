0d99e14ab447745bbc8df8b9082a5af1f578f9f6
Kauê Murakami <kauetmurakami@gmail.com>
2020-05-27 20:16:11 -0300

    finalizando etapa de construção, e iniciando alteração de tema

--- lib/blocs/weather_bloc/weather_bloc.dart
+++ lib/blocs/weather_bloc/weather_bloc.dart
@@ -12,7 +12,7 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   final WeatherRepository weatherRepository;
 
   WeatherBloc({@required this.weatherRepository})
-  :assert(weatherRepository != null);
+      : assert(weatherRepository != null);
 
   @override
   WeatherState get initialState => WeatherEmpty();
@@ -21,15 +21,29 @@ class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
   Stream<WeatherState> mapEventToState(
     WeatherEvent event,
   ) async* {
-    if(event is FetchWeather){
-      yield WeatherLoading();
-      try{
-        final Weather weather = await weatherRepository.getWeather(event.city);
-        yield WeatherLoaded(weather: weather);
-      }catch(_){
-        yield WeatherError();
-      }
+    if (event is FetchWeather) {
+      yield* _mapFetchWeatherToState(event);
+    } else if (event is RefreshWeather) {
+      yield* _mapRefreshWeatherToState(event);
     }
-
   }
+  
+  Stream<WeatherState> _mapFetchWeatherToState(FetchWeather event) async* {
+  yield WeatherLoading();
+  try {
+    final Weather weather = await weatherRepository.getWeather(event.city);
+    yield WeatherLoaded(weather: weather);
+  } catch (_) {
+    yield WeatherError();
+  }
+}
+
+Stream<WeatherState> _mapRefreshWeatherToState(RefreshWeather event) async* {
+  try {
+    final Weather weather = await weatherRepository.getWeather(event.city);
+    yield WeatherLoaded(weather: weather);
+  } catch (_) {
+    yield state;
+  }
+}
 }
